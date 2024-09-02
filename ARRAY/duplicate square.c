375b2b6e6c44bcb6e8bcaf79725aa0e2d5a3a867
vijaistark <vijayaraja101125@gmail.com>
2024-09-02 11:41:12 +0530

    main

---
+++ ARRAY/duplicate square.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int arr[20];
+    int n;
+    scanf("%d",&n);
+    for(int i =0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    for(int i =0;i<n;i++){
+    for(int j =i+1;j<n;j++){
+        if(arr[i] == arr[j]){
+            arr[i] = arr[i] * arr[i];
+            arr[j] = arr[j] * arr[j];
+        }
+    }
+    
+    
+    }
+    for(int i =0;i<n;i++){
+        printf("%d ",arr[i]);
+}
+}
